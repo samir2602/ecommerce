35a6e20aa1aad27c5b95f2f60ecfc02aa9d5ecb6
Samir <120911119+samir2602@users.noreply.github.com>
2026-05-20 11:32:14 +0530

    Fixed DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
 
 class DatabaseSeeder extends Seeder
 {
