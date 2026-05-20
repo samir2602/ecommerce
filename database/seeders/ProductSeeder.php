c010da6c73d13a9eda26ac73ec587f9e6a32d8f9
Samir <120911119+samir2602@users.noreply.github.com>
2026-05-20 11:44:30 +0530

    Fixed ProductSeeder imports

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Support\Str;
 
 class ProductSeeder extends Seeder
 {
